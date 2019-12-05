58b90f0c2e7ec8d94009ccb00602b2de16acee95
George Chilian <GChilian@britannia-superfine.com>
2019-12-05 09:06:45 +0000

    Initial Commit

---
+++ CoPilot/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CoPilot
+//
+//  Created by George Chilian on 05/12/2019.
+//  Copyright © 2019 George Chilian. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoPilot/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CoPilot
+//
+//  Created by George Chilian on 05/12/2019.
+//  Copyright © 2019 George Chilian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
