973a4670eb5e2316d4093683d588f778556d7a0e
George Chilian <GChilian@britannia-superfine.com>
2019-12-05 14:38:55 +0000

    Alpha 0.1

---
+++ CoPilot/LocationSearchTable.swift
@@ -0,0 +1,46 @@
+//
+//  LocationSearchTable.swift
+//  CoPilot
+//
+//  Created by George Chilian on 05/12/2019.
+//  Copyright © 2019 George Chilian. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationSearchTable : UITableViewController {
+    var matchingItems:[MKMapItem] = []
+    var mapView: MKMapView? = nil
+}
+
+extension LocationSearchTable : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.start { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension LocationSearchTable {
+    override func tableView(_ tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell"){ 
+            return cell }
+        else { let cell = UITableViewCell()
+            return cell }
+}
+}
--- CoPilot/SecondViewController.swift
+++ CoPilot/SecondViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import MapKit
 
 class SecondViewController: UIViewController {
 
+    var resultSearchController:UISearchController? = nil
+    
+    @IBOutlet weak var mapView: MKMapView!
+    fileprivate let locationManager:CLLocationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Find User Location
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+
+        //Zoom to User Location
+        if let userLocation = locationManager.location?.coordinate {
+        let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 200, longitudinalMeters: 200)
+        mapView.setRegion(viewRegion, animated: false)
+            
+        //Set up Search Results table
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        //Set up Search Bar
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Destinations..."
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        //Configure UISearchController appearance
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        definesPresentationContext = true
+            
+        locationSearchTable.mapView = mapView
+            
     }
 
 
 }
 
+}
+
+extension SecondViewController : CLLocationManagerDelegate {
+    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+
+    
+    private func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error:: \(error)")
+    }
+}
